b10089282e4a940fe2ce2e9b83eca835b4c3a4d8
Ang3el7z <ang3el7z@gmail.com>
2025-07-25 23:44:42 +0300

    delete enter

--- other/install-singbox.sh
+++ other/install-singbox.sh
@@ -47,7 +47,7 @@ show_warning() {
 }
 
 show_message() {
-    echo -e "${FG_USER_COLOR}${INDENT}${ARROW} $1${RESET}\n"
+    echo -e "${FG_USER_COLOR}${INDENT}${ARROW} $1${RESET}"
 }
 
 read_input() {
